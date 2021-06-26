8017a0bd220f022217f67c2e1be40e2968cad422
harshaltribhuwan <harshaltribhuwan12345@gmail.com>
2021-06-27 00:18:17 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,257 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning:
+        "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+    },
+    {
+      id: 5,
+      emoji: "😘",
+      name: "Face Blowing a Kiss",
+      meaning:
+        "A yellow face winking with puckered lips blowing a kiss, depicted as a small, red heart. May represent a kiss goodbye or good night and convey feelings of love and affection more generally."
+    },
+    {
+      id: 6,
+      emoji: "😝",
+      name: "Squinting Face with Tongue",
+      meaning:
+        "A yellow face with scrunched, X-shaped eyes and a big grin, sticking out its tongue. Often conveys a sense of fun, excitement, playfulness, hilarity, and happiness, as if saying Squee! or Awesome!."
+    },
+    {
+      id: 7,
+      emoji: "🙃",
+      name: "Upside-Down Face",
+      meaning:
+        "A classic smiley, turned upside down. Implemented as a flipped version of 🙂 Slightly Smiling Face on most platforms."
+    },
+    {
+      id: 8,
+      emoji: "😗",
+      name: "Kissing Face",
+      meaning:
+        "A yellow face with simple, open eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection."
+    },
+    {
+      id: 9,
+      emoji: "😋",
+      name: "Face Savoring Food",
+      meaning:
+        "A yellow face with smiling eyes and a broad, closed smile with its tongue sticking out of one corner, as if licking its lips in appetite or contentment. Widely used to convey that a food item is delicious. May also express that a person is attractive."
+    },
+    {
+      id: 10,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning:
+        "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+    },
+    {
+      id: 11,
+      emoji: "🤔",
+      name: "Thinking Face",
+      meaning:
+        "A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought. Often used to question or scorn something ."
+    },
+    {
+      id: 12,
+      emoji: "😎",
+      name: "Smiling Face with Sunglasses",
+      meaning:
+        "A yellow face with a broad, closed smile wearing black sunglasses, as if a pair of classic Wayfarers. Often used to convey the slang sense of cool. May also express a confident, carefree attitude or that something is excellent."
+    },
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning:
+        "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+    },
+    {
+      id: 5,
+      emoji: "😘",
+      name: "Face Blowing a Kiss",
+      meaning:
+        "A yellow face winking with puckered lips blowing a kiss, depicted as a small, red heart. May represent a kiss goodbye or good night and convey feelings of love and affection more generally."
+    },
+    {
+      id: 6,
+      emoji: "😝",
+      name: "Squinting Face with Tongue",
+      meaning:
+        "A yellow face with scrunched, X-shaped eyes and a big grin, sticking out its tongue. Often conveys a sense of fun, excitement, playfulness, hilarity, and happiness, as if saying Squee! or Awesome!."
+    },
+    {
+      id: 7,
+      emoji: "🙃",
+      name: "Upside-Down Face",
+      meaning:
+        "A classic smiley, turned upside down. Implemented as a flipped version of 🙂 Slightly Smiling Face on most platforms."
+    },
+    {
+      id: 8,
+      emoji: "😗",
+      name: "Kissing Face",
+      meaning:
+        "A yellow face with simple, open eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection."
+    },
+    {
+      id: 9,
+      emoji: "😋",
+      name: "Face Savoring Food",
+      meaning:
+        "A yellow face with smiling eyes and a broad, closed smile with its tongue sticking out of one corner, as if licking its lips in appetite or contentment. Widely used to convey that a food item is delicious. May also express that a person is attractive."
+    },
+    {
+      id: 10,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning:
+        "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+    },
+    {
+      id: 11,
+      emoji: "🤔",
+      name: "Thinking Face",
+      meaning:
+        "A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought. Often used to question or scorn something ."
+    },
+    {
+      id: 12,
+      emoji: "😎",
+      name: "Smiling Face with Sunglasses",
+      meaning:
+        "A yellow face with a broad, closed smile wearing black sunglasses, as if a pair of classic Wayfarers. Often used to convey the slang sense of cool. May also express a confident, carefree attitude or that something is excellent."
+    },
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning:
+        "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+    },
+    {
+      id: 5,
+      emoji: "😘",
+      name: "Face Blowing a Kiss",
+      meaning:
+        "A yellow face winking with puckered lips blowing a kiss, depicted as a small, red heart. May represent a kiss goodbye or good night and convey feelings of love and affection more generally."
+    },
+    {
+      id: 6,
+      emoji: "😝",
+      name: "Squinting Face with Tongue",
+      meaning:
+        "A yellow face with scrunched, X-shaped eyes and a big grin, sticking out its tongue. Often conveys a sense of fun, excitement, playfulness, hilarity, and happiness, as if saying Squee! or Awesome!."
+    },
+    {
+      id: 7,
+      emoji: "🙃",
+      name: "Upside-Down Face",
+      meaning:
+        "A classic smiley, turned upside down. Implemented as a flipped version of 🙂 Slightly Smiling Face on most platforms."
+    },
+    {
+      id: 8,
+      emoji: "😗",
+      name: "Kissing Face",
+      meaning:
+        "A yellow face with simple, open eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection."
+    },
+    {
+      id: 9,
+      emoji: "😋",
+      name: "Face Savoring Food",
+      meaning:
+        "A yellow face with smiling eyes and a broad, closed smile with its tongue sticking out of one corner, as if licking its lips in appetite or contentment. Widely used to convey that a food item is delicious. May also express that a person is attractive."
+    },
+    {
+      id: 10,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning:
+        "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."
+    },
+    {
+      id: 11,
+      emoji: "🤔",
+      name: "Thinking Face",
+      meaning:
+        "A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought. Often used to question or scorn something ."
+    },
+    {
+      id: 12,
+      emoji: "😎",
+      name: "Smiling Face with Sunglasses",
+      meaning:
+        "A yellow face with a broad, closed smile wearing black sunglasses, as if a pair of classic Wayfarers. Often used to convey the slang sense of cool. May also express a confident, carefree attitude or that something is excellent."
+    }
+  ];
+  
+  export default emojipedia;
+  
